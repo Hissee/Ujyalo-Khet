072364c25435f88a365c6230b98b7302015fc2bd
reekishashrestha-00 <reekishashrestha@gmail.com>
2025-07-30 15:03:40 +0545

    added the header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:'about',component: AboutComponent},
+    {path:'login',component:LoginComponent},
+    {path:'contact',component:ContactComponent},
+    {path:'',component:HomeComponent},
+    {path:'**',component:PagenotfoundComponent}
+
+
+];

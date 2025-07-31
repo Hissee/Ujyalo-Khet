5117ef8742462833278baf7dee7c68b6f61f8655
Hissee <maharjanhissee@gmail.com>
2025-07-31 11:33:15 +0545

    resolving Conflict in route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,12 @@ import { Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { SignupKhetaloComponent } from './signup-khetalo/signup-khetalo.component';
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
 import { ProductListComponent } from './product-list/product-list.component';
-=======
-import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
->>>>>>> eb713a56792ba2ef718409b9f5d944ab832720ec
+
 
 export const routes: Routes = [
     {
@@ -29,14 +25,17 @@ export const routes: Routes = [
     {
         path: 'product-list',
         component: ProductListComponent
+    },
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    {
+        path:'contact',
+        component:ContactComponent
+    },
+    {
+        path:'**',
+        component:PagenotfoundComponent
     }
-];
-export const routes: Routes = [
-    {path:'about',component: AboutComponent},
-    {path:'login',component:LoginComponent},
-    {path:'contact',component:ContactComponent},
-    {path:'',component:HomeComponent},
-    {path:'**',component:PagenotfoundComponent}
-
-
 ];

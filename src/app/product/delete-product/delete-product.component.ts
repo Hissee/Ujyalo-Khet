e9ed1accf4a7003c40cedb3b95c62be40edd7105
reekishashrestha-00 <reekishashrestha@gmail.com>
2025-09-02 13:23:48 +0545

    setup routing of add,delete,update product

---
+++ src/app/product/delete-product/delete-product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-delete-product',
+  imports: [RouterLink],
+  templateUrl: './delete-product.component.html',
+  styleUrl: './delete-product.component.css'
+})
+export class DeleteProductComponent {
+
+}
---
+++ src/app/product/update-product/update-product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-update-product',
+  imports: [RouterLink],
+  templateUrl: './update-product.component.html',
+  styleUrl: './update-product.component.css'
+})
+export class UpdateProductComponent {
+
+}

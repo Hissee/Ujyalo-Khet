4e381db167c81bc34acd1682dd80599402df3571
Hissee <maharjanhissee@gmail.com>
2025-11-01 12:39:54 +0545

    signup successful

--- src/app/signup-component/signup-consumer/signup-consumer.component.ts
+++ src/app/signup-component/signup-consumer/signup-consumer.component.ts
@@ -2,6 +2,7 @@ import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AuthService} from '../auth.service';
+import {User} from '../IUser';
 
 @Component({
   selector: 'app-signup-consumer',
@@ -47,23 +48,40 @@ export class SignupConsumerComponent implements OnInit, OnDestroy {
 
   constructor(private http: HttpClient) {}
 
-  onSubmit() {
-    this.successMessage = '';
-    this.errorMessage = '';
-
-    this.service.signup(this.formData)
-      .subscribe({
-      next: (res: any) => {
-        this.successMessage = res.message || 'Signup successful!';
-        this.formData = { firstName: '', middleName: '', lastName: '', email: '', phone: '', province: '', city: '', street: '', password: '', role: 'consumer' };
-      },
-      error: (err) => {
-        this.errorMessage = err.error?.message || 'Signup failed. Please try again.';
-      }
-    });
-  }
-
   private initForm() {
     this.userForm = this.service.createUpdateForm();
   }
+
+  onSubmit() {
+
+    this.successMessage = '';
+    this.errorMessage = '';
+    const user:User = {
+      firstName: this.userForm.get('firstName')?.value,
+      middleName: this.userForm.get('middleName')?.value,
+      lastName: this.userForm.get('lastName')?.value,
+      email: this.userForm.get('email')?.value,
+      phone: this.userForm.get('phone')?.value,
+      role: 'consumer',
+      province: this.userForm.get('province')?.value,
+      city: this.userForm.get('city')?.value,
+      street: this.userForm.get('street')?.value,
+      password: this.userForm.get('password')?.value,
+    };
+    console.log(user);
+
+
+
+    this.service.signup(user)
+      .subscribe({
+        next: (res: any) => {
+          this.successMessage = res.message || 'Signup successful!';
+          this.formData = { firstName: '', middleName: '', lastName: '', email: '', phone: '', province: '', city: '', street: '', password: '', role: 'consumer' };
+        },
+        error: (err) => {
+          this.errorMessage = err.error?.message || 'Signup failed. Please try again.';
+        }
+      });
+  }
+
 }

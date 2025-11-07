60379267dbafd9fd4a89ff91b23e3960b968b61b
Hissee <maharjanhissee@gmail.com>
2025-11-08 00:47:48 +0545

    WIP

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ujyalo-backend.vercel.app/api',
+  apiUrl: 'https://ujyalo-backend.vercel.app',
   esewaSecret: '8gBm/:&EnhH.1/q' // Replace with your actual eSewa secret key
 };
 
